1a8ef05af499d36747d670369a51434d9094b63b
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-03-24 22:18:54 -0600

    Add files via upload

---
+++ Sumadelaserie/Sumadelaserie/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sumadelaserie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            operacion x  = new operacion();
+            int _numero = int.Parse(textBox1.Text);
+            x.op(_numero);
+            MessageBox.Show(":" + x, " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Sumadelaserie/Sumadelaserie/operacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sumadelaserie
+{
+
+    class operacion
+    {
+        // declaracion de propiedad
+        private int numero;
+
+        public int Numero
+        { get => numero; 
+          set => numero = value;
+        }
+
+        public void op(int _numero)
+        {
+            double resultado = 0;
+
+            for (int i = 1; i <= _numero; i++)
+            {
+                resultado = resultado + (1 / i);
+
+                
+            }
+            
+        }
+
+    }
+}
